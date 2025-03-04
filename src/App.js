f7f6b17b5e5050b3d0d8ae6d6c07c55d19cd4134
Jason Dhekial Pyrbot <jasonvlog81@gmail.com>
2025-03-04 12:01:30 +0530

    Initial commit: Added Joke App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Joke from './components/Joke';
+import Header from './components/Header';
 
 function App() {
   return (
+    <Header />
     <div className="App">
      <h1>Hi generate  a joke using react and Joke Api</h1>
      <Joke/>
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Header.css";  // Import external CSS file
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="title">😂 Joke App</h1>
+      <nav>
+        <ul className="nav-list">
+          <li><a href="#" className="nav-item">Home</a></li>
+          <li><a href="#" className="nav-item">Jokes</a></li>
+          <li><a href="#" className="nav-item">About</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
+
